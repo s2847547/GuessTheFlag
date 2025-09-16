a4b9b9176e87374d5001b11f516e9112d7b3d73e
s2847547 <s2847547@students.fscj.edu>
2025-09-16 19:56:05 -0400

    Showing alert messages

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        Button {
-            print("Button was tapped.")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
+        Button("Show alert") {
+            showingAlert = true
         }
-    }
-
-    func executeDelete() {
-        print("Now deleting...")
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this.")
+        }
     }
 }
 
