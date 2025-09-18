4a706fe94f70a2f71a715d06900661d2e82f49f2
s2847547 <s2847547@students.fscj.edu>
2025-09-18 15:19:16 -0400

    Styling our flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,31 +12,35 @@ struct ContentView: View {
         "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland",
         "Spain", "UK", "Ukraine", "US",
     ].shuffled()
-
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
-
+    
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
-
+            
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
-
+                        .font(.subheadline.weight(.heavy))
+                    
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
-
+                
                 ForEach(0..<3) { number in
                     Button {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
             }
